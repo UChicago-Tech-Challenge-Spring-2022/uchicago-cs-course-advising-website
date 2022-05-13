a0fbb5d8a87fc1aefabd1012778ac3632c42cd79
JihuiTanUchicago <tanjihui@uchicago.edu>
2022-05-13 21:31:15 +0000

    sign in/up/out functions complete, and display is handled properly

--- app/controllers/notify_controller.rb
+++ app/controllers/notify_controller.rb
@@ -1,7 +1,11 @@
 class NotifyController < ApplicationController
 
   def notification
-    render({ :template => "notify.html.erb" })
+    if user_signed_in?
+      render({ :template => "notify.html.erb" })
+    else
+      redirect_to "/", alert: "In order to use this feature, you have to sign in first"
+    end
   end
-  
-  end
+
+end
--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,7 +1,11 @@
 class ScheduleController < ApplicationController
 
   def schedule
-    render({ :template => "schedule.html.erb" })
+    if user_signed_in?
+      render({ :template => "schedule.html.erb" })
+    else
+      redirect_to "/", alert: "In order to use this feature, you have to sign in first"
+    end
   end
   
 end
