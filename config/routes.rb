ccaac877803df42cf67408f7771de78816db454d
JihuiTanUchicago <tanjihui@uchicago.edu>
2022-05-14 23:06:29 +0000

    feature 1 complete!

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-root to: 'home#index'
-       
-get '/manual', to: 'manual#index'
 
-get '/schedule', to: 'schedule#index'
-
-get '/notification', to: 'notification#index'
-post '/notificaiton', to: 'notification#create_monitor'
 
 end
