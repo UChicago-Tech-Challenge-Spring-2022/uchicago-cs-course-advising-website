fe48ac164e35306893774e14518532b1d5314d2a
JihuiTanUchicago <tanjihui@uchicago.edu>
2022-05-15 11:42:01 +0000

    change notify.html.erb to notification...

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -2,7 +2,7 @@ class NotificationController < ApplicationController
 
   def index
     if user_signed_in?
-      render({ :template => "notify.html.erb" })
+      render({ :template => "notification.html.erb" })
     else
       redirect_to "/", alert: "In order to use the Notifier, you have to sign in first"
     end
