fc04fc4109e661cc9e85e0aa0c75bf0b18bfccea
JihuiTanUchicago <tanjihui@uchicago.edu>
2022-05-25 10:41:00 +0000

    manual finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 root to: 'home#index'
-       
-get '/manual', to: 'manual#index'
+
 
 get '/schedule', to: 'schedule#index'
 
